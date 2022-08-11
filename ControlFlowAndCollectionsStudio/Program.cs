fc154d9bdc88ec7660b11a97314046838e45482a
Jordan <jordan_anthony11@aol.com>
2022-08-10 20:38:15 -0500

    Add project files.

---
+++ ControlFlowAndCollectionsStudio/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Please enter a sentence:");
+string userInput = Console.ReadLine();
+string sentence = userInput.ToLower();
+
+Dictionary<char, int> CharacterCount = new Dictionary<char, int>();
+
+foreach (char letter in sentence)
+{
+    if (!CharacterCount.ContainsKey(letter))
+    {
+        CharacterCount.Add(letter, 1);
+    }
+    else
+    {
+        CharacterCount[letter] = CharacterCount[letter] + 1;
+    }
+}
+
+foreach (char letter in CharacterCount.Keys)
+{
+    Console.WriteLine($"{letter}: {CharacterCount[letter]}");
+}
